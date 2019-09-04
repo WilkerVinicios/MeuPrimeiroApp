3b4de1b096fee94c0d86af36785e4baa5c1448db
Wilker <willkervinicios@gmail.com>
2019-09-04 19:56:31 -0300

    Primeira Versão de Commit.

---
+++ Desktop/app/src/main/java/br/senac/go/wilkervinicios/PrimeiraTela.java
@@ -0,0 +1,31 @@
+package br.senac.go.wilkervinicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PrimeiraTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira_tela);
+
+
+        Button openBT = findViewById(R.id.button2);
+        openBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CronogramaActivity.class);
+                intent.putExtra("teste",R.id.textView6);
+                startActivity(intent);
+            }
+        });
+    }
+}
